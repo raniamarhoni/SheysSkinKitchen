6441f9425b345e45e1e967d5676446c0cf7e80d4
raniamarhoni <marhoni@outlook.com>
2021-05-06 16:43:12 +0000

    adding editing and deleteing products added custome clearable file

--- products/urls.py
+++ products/urls.py
@@ -7,9 +7,8 @@ urlpatterns = [
     path(
         '<int:product_id>/<int:size_id>/', views.product_size,
         name='product_size'),
-        path('add/', views.add_product, name='add_product'),
-        path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
-        path('delete/<int:product_id>/',
-         views.delete_product,
-         name='delete_product'),
+    path('add/', views.add_product, name='add_product'),
+    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
+    path('delete/<int:product_id>/',
+         views.delete_product, name='delete_product'),
 ]
