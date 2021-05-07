85fff16bc40598e59821269c495952f70d06079c
raniamarhoni <marhoni@outlook.com>
2021-05-07 01:56:07 +0000

    requirements.txt updated

--- sheysskinkitchen/settings.py
+++ sheysskinkitchen/settings.py
@@ -13,11 +13,12 @@ from pathlib import Path
 if os.path.exists("env.py"):
     import env
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
